09118bdb6ac0757ac256bf08570a060d71edaa4c
aguslasalvia <agus.blumenfeld13@gmail.com>
2026-07-21 20:45:39 -0300

    feat: search videos &&  yt audio stream separeted

--- server/routes/song_router.py
+++ server/routes/song_router.py
@@ -6,5 +6,10 @@ router = APIRouter()
 
 @router.get('/search')
 async def search(q: str, current_user: dict = Depends(get_current_user)):
-    result = await service.search_video_urls(q)
-    return result
+    return await service.search_songs(q)
+    
+
+
+@router.get('/stream-url')
+async def stream_url(video_id:str,current_user:dict = Depends(get_current_user)):
+    return service.get_audio_stream_url(video_id)
---
+++ server/youtube/search.py
@@ -0,0 +1,19 @@
+from youtubesearchpython import VideosSearch
+from typing import Optional, Dict, Any, Union
+
+
+async def search_video_urls(filter_text: str, limit: int = 25) -> Optional[Union[Dict[str, Any], str]]:
+    try:
+        search_result = VideosSearch(filter_text.strip(), limit=limit).result()
+
+        if isinstance(search_result, dict):
+            return search_result
+        elif isinstance(search_result, str):
+            return search_result
+        else:
+            return None
+
+    except ValueError:
+        return None
+    except Exception:
+        return None
